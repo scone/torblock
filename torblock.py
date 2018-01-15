3856b901909e1c86511fbffeebcc0e197001a798
Sam <samjcleveland@gmail.com>
2018-01-15 01:45:17 -0600

    Bug Fix
    Fixing bug. I wasn’t checking for the nginx flag before the loop began.
    If it’s not present then we are outputting to iptables. Flush the rules
    first, then input them.

--- torblock.py
+++ torblock.py
@@ -8,9 +8,9 @@
 #
 
 # Description:  This script attempts to block all known tor exit nodes (as
-#		reported by the Tor Project's website) from communicating
-#		with the server that it is run on using iptables firewalling
-#		rules.
+#       reported by the Tor Project's website) from communicating
+#       with the server that it is run on using iptables firewalling
+#       rules.
 
 import sys
 import re
@@ -22,7 +22,7 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description='TorBlock - Block Tor Exit Nodes with IPtables or Nginx.')
     parser.add_argument('server_ip', help='IP address of the server where the blocking will occur.')
-    parser.add_argument('--nginx', metavar="Filename", default="tor_exits.conf", type=argparse.FileType("a"), help='Filename to output the Nginx configuration file to.')
+    parser.add_argument('--nginx', metavar="Filename", type=argparse.FileType("a"), help='Filename to output the Nginx configuration file to.')
     return parser.parse_args()
 
 # Execute iptables command to block a node after sanity checking
@@ -48,7 +48,14 @@ def block_nginx(ip, output_file):
 # calls the usage() function to print the usage information and exit the
 # program.
 args = parse_args()
-print("Blocking all tor exit nodes.")
+
+if not args.nginx:
+    try:
+        subprocess.check_call(['iptables', '-F', 'INPUT'])
+    except OSError as e:
+        if (e[0] == errno.EPERM):
+            print("Since this script modifies the firewall with iptables it must be run with root privileges.", file=sys.stderr)
+            sys.exit(1)
 
 print("Retrieving list of nodes from Tor project website.")
 ip_list_url = "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip={server_ip}"
